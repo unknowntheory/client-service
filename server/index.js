0417f031d466ab74134a3b1a0db6e30ec4021997
Daniel London <theryy408@gmail.com>
2017-12-14 10:13:44 -0800

    added elastic search server

--- server/index.js
+++ server/index.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-var fake = require('./faker.js');
+//var fake = require('./faker.js'); mongo
+const elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+});
+
 
 //var easak = require('greatness');//sike
 // fake.fakerUser() //generate fake users
 const app = express();
-for (var i = 0; i < 1000; i++) {
-  fake.fakeProduct();
-}
-
-
+// for (var i = 0; i < 1000; i++) {
+//   fake.fakeProduct();
+// }
+client.ping({
+  // ping usually has a 3000ms timeout
+  requestTimeout: 60000
+}, function (error) {
+  if (error) {
+    console.trace('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
+});
+// client.search({
+//   q: 'pants'
+// }).then(function (body) {
+//   var hits = body.hits.hits;
+// }, function (error) {
+//   console.trace(error.message);
+// });
+//
+//
 
 
 
